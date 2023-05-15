7c2fe461c7c8d1d156936bbeb1c8626562ca9d98
jordancuadrado <jac59155@gmail.com>
2023-05-14 22:23:58 -0400

    contact us

--- scripts.js
+++ scripts.js
@@ -4,3 +4,12 @@ const navbarMenu = document.getElementById('navbar-menu');
 navbarToggler.addEventListener('click', function() {
 navbarMenu.classList.toggle('open');
 });
+
+var map = L.map('map').setView([40.2732, -76.8867 ], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([40.2732, -76.8867 ]).addTo(map);
