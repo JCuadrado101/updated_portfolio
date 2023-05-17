fe79763d6a0f9f691e29fc1c0dd1f026e4990799
jordancuadrado <jac59155@gmail.com>
2023-05-16 20:21:28 -0400

    contact page

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
+// NAVBAR
+
 const navbarToggler = document.getElementById('navbar-toggler');
 const navbarMenu = document.getElementById('navbar-menu');
 
 navbarToggler.addEventListener('click', function() {
 navbarMenu.classList.toggle('open');
 });
+
+// MAP
 
 var map = L.map('map').setView([40.2732, -76.8867 ], 13);
 
@@ -13,3 +17,31 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
 }).addTo(map);
 
 var marker = L.marker([40.2732, -76.8867 ]).addTo(map);
+
+// SMS
+
+const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+
+    fetch('http://localhost:3000/send-sms', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        to: '+17173804118',
+            message: ` ${name} \n ${email} \n ${message}`,
+        }),
+        })
+    .then(response => response.text())
+    .then(result => {
+      console.log(result);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
